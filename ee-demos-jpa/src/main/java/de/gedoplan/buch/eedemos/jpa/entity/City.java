6047e4ad715528c63f24b98f7ed8a89626c7bf53
Dirk Weil <dirk.weil@gedoplan.de>
2013-05-02 17:05:50 +0200

    Entity-Basisklasse eingesetzt

--- ee-demos-jpa/src/main/java/de/gedoplan/buch/eedemos/jpa/entity/City.java
+++ ee-demos-jpa/src/main/java/de/gedoplan/buch/eedemos/jpa/entity/City.java
@@ -1,30 +1,29 @@
 package de.gedoplan.buch.eedemos.jpa.entity;
 
+import de.gedoplan.baselibs.persistence.entity.GeneratedIntegerIdEntity;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.ColumnResult;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.NamedNativeQuery;
 import javax.persistence.SqlResultSetMapping;
 import javax.persistence.Table;
-import javax.persistence.TableGenerator;
 
 @Entity
 @Access(AccessType.FIELD)
 @NamedNativeQuery(name = "City_populationDensity", query = "SELECT NAME, POPULATION/AREA AS DENSITY FROM EEDEMOS_CITY", resultSetMapping = "City_populationDensity")
 @SqlResultSetMapping(name = "City_populationDensity", columns = { @ColumnResult(name = "NAME"), @ColumnResult(name = "DENSITY") })
 @Table(name = City.TABLE_NAME)
-public class City
+public class City extends GeneratedIntegerIdEntity
 {
   public static final String TABLE_NAME = "EEDEMOS_CITY";
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.TABLE, generator = "cityGenerator")
-  @TableGenerator(name = "cityGenerator", table = "EEDEMOS_CITY_GEN", pkColumnName = "GENERATOR", valueColumnName = "NEXT_ID", initialValue = 200000)
-  private Integer            id;
+  // Durch die Ableitung von GeneratedIntegerIdEntity lässt sich dieser Code einsparen
+  //  @Id
+  //  @GeneratedValue(strategy = GenerationType.TABLE, generator = "cityGenerator")
+  //  @TableGenerator(name = "cityGenerator", table = "EEDEMOS_CITY_GEN", pkColumnName = "GENERATOR", valueColumnName = "NEXT_ID", initialValue = 200000)
+  //  private Integer            id;
   private String             name;
 
   private int                population;
@@ -42,6 +41,7 @@ public class City
     this.area = area;
   }
 
+  @Override
   public Integer getId()
   {
     return this.id;
@@ -77,38 +77,39 @@ public class City
     this.area = area;
   }
 
-  @Override
-  public int hashCode()
-  {
-    return this.id != null ? this.id.hashCode() : 0;
-  }
-
-  @Override
-  public boolean equals(Object obj)
-  {
-    if (this == obj)
-    {
-      return true;
-    }
-
-    if (obj == null)
-    {
-      return false;
-    }
-
-    if (getClass() != obj.getClass())
-    {
-      return false;
-    }
-
-    final City other = (City) obj;
-
-    return this.id != null && this.id.equals(other.id);
-  }
-
-  @Override
-  public String toString()
-  {
-    return "City [id=" + this.id + ", name=" + this.name + ", population=" + this.population + ", area=" + this.area + "]";
-  }
+  // Durch die Ableitung von GeneratedIntegerIdEntity lässt sich dieser Code einsparen
+  //  @Override
+  //  public int hashCode()
+  //  {
+  //    return this.id != null ? this.id.hashCode() : 0;
+  //  }
+  //
+  //  @Override
+  //  public boolean equals(Object obj)
+  //  {
+  //    if (this == obj)
+  //    {
+  //      return true;
+  //    }
+  //
+  //    if (obj == null)
+  //    {
+  //      return false;
+  //    }
+  //
+  //    if (getClass() != obj.getClass())
+  //    {
+  //      return false;
+  //    }
+  //
+  //    final City other = (City) obj;
+  //
+  //    return this.id != null && this.id.equals(other.id);
+  //  }
+  //
+  //  @Override
+  //  public String toString()
+  //  {
+  //    return "City [id=" + this.id + ", name=" + this.name + ", population=" + this.population + ", area=" + this.area + "]";
+  //  }
 }
