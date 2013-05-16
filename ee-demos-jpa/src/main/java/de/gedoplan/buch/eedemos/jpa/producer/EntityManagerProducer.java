43b6b70846ba182f2711a361b0ec5234614e59fa
Dirk Weil <dirk.weil@gedoplan.de>
2013-05-16 10:14:54 +0200

    Logging eingebaut

--- ee-demos-jpa/src/main/java/de/gedoplan/buch/eedemos/jpa/producer/EntityManagerProducer.java
+++ ee-demos-jpa/src/main/java/de/gedoplan/buch/eedemos/jpa/producer/EntityManagerProducer.java
@@ -2,8 +2,11 @@ package de.gedoplan.buch.eedemos.jpa.producer;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import org.apache.commons.logging.Log;
 
 /**
  * Producer für einen transaktionsgebundenen Entity Manager.
@@ -14,6 +17,19 @@ import javax.persistence.PersistenceContext;
 public class EntityManagerProducer
 {
   @PersistenceContext(unitName = "ee-demos")
+  private EntityManager entityManager;
+
+  @Inject
+  private Log           log;
+
   @Produces
-  EntityManager entityManager;
+  public EntityManager createEntityManager()
+  {
+    if (this.log.isTraceEnabled())
+    {
+      this.log.trace("createEntityManager: " + this.entityManager + " (flushMode=" + this.entityManager.getFlushMode() + ", properties=" + this.entityManager.getProperties() + ")");
+    }
+
+    return this.entityManager;
+  }
 }
