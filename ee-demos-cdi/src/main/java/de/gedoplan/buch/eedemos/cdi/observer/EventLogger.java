785130cac2ef8f032046809add4d36f35516a035
Dirk Weil <dirk.weil@gedoplan.de>
2013-06-10 12:56:05 +0200

    EventMetaData (noch auskommentiert)

--- ee-demos-cdi/src/main/java/de/gedoplan/buch/eedemos/cdi/observer/EventLogger.java
+++ ee-demos-cdi/src/main/java/de/gedoplan/buch/eedemos/cdi/observer/EventLogger.java
@@ -6,12 +6,21 @@ import org.apache.commons.logging.Log;
 
 public class EventLogger
 {
-  public static void logEvent(@Observes Object event, Log logger)
+  public static void logEvent(@Observes Object event, Log log)
   {
-    if (logger.isDebugEnabled())
+    if (log.isTraceEnabled())
     {
-      logger.debug("Event: " + event);
+      log.trace("Event: " + event);
     }
   }
+
+  // TODO: Injektion von EventMetaData in GLF-4.0-b86 schlägt fehl
+  //  public static void logEvent(@Observes Object event, EventMetadata eventMetadata, Log log)
+  //  {
+  //    if (log.isTraceEnabled())
+  //    {
+  //      log.trace("Event: " + eventMetadata.getQualifiers() + " " + event);
+  //    }
+  //  }
 
 }
