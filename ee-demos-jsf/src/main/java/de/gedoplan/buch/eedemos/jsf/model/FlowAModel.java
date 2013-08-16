547f81cf78796e1861f303f3d0f44d57abb09751
Dirk Weil <dirk.weil@gedoplan.de>
2013-08-16 07:51:02 +0200

    Noch'n Flow

---
+++ ee-demos-jsf/src/main/java/de/gedoplan/buch/eedemos/jsf/model/FlowAModel.java
@@ -0,0 +1,10 @@
+package de.gedoplan.buch.eedemos.jsf.model;
+
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+@Named
+@FlowScoped("flowA")
+public class FlowAModel extends FlowModelBase
+{
+}
