4c607bf8e03ab7e762d2ef1bb9ede28c4e5d3434
Dirk Weil <dirk.weil@gedoplan.de>
2013-08-17 18:40:06 +0200

    Flow-Definitionen gefixt

--- ee-demos-jsf/src/main/java/de/gedoplan/buch/eedemos/jsf/model/Flow1Model.java
+++ ee-demos-jsf/src/main/java/de/gedoplan/buch/eedemos/jsf/model/Flow1Model.java
@@ -1,10 +1,30 @@
 package de.gedoplan.buch.eedemos.jsf.model;
 
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
 import javax.faces.flow.FlowScoped;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.builder.FlowDefinition;
 import javax.inject.Named;
 
 @Named
 @FlowScoped("flow1")
 public class Flow1Model extends FlowModelBase
 {
+  @Produces
+  @FlowDefinition
+  public static Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder)
+  {
+
+    flowBuilder.id("", "flow1");
+
+    flowBuilder.viewNode("flow1", "/flow1/flow1a.xhtml").
+        markAsStartNode();
+
+    flowBuilder.returnNode("exitFlow").fromOutcome("exitFlow");
+
+    return flowBuilder.getFlow();
+  }
+
 }
--- ee-demos-jsf/src/main/java/de/gedoplan/buch/eedemos/jsf/model/Flow3Model.java
+++ ee-demos-jsf/src/main/java/de/gedoplan/buch/eedemos/jsf/model/Flow3Model.java
@@ -1,10 +1,29 @@
 package de.gedoplan.buch.eedemos.jsf.model;
 
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
 import javax.faces.flow.FlowScoped;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.builder.FlowDefinition;
 import javax.inject.Named;
 
 @Named
 @FlowScoped("flow3")
 public class Flow3Model extends FlowModelBase
 {
+  @Produces
+  @FlowDefinition
+  public static Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder)
+  {
+    flowBuilder.id("", "flow3");
+
+    flowBuilder.viewNode("flow3a", "/flow/flow3/flow3a.xhtml").markAsStartNode();
+
+    flowBuilder.viewNode("flow3b", "/flow/flow3/flow3b.xhtml");
+
+    flowBuilder.returnNode("exitFlow").fromOutcome("exitFlow");
+
+    return flowBuilder.getFlow();
+  }
 }
