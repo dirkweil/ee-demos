5ac500d167d9059968a223080d8613995cd9cb6d
Dirk Weil <dirk.weil@gedoplan.de>
2013-08-21 23:00:18 +0200

    .

--- ee-demos-jsf/src/main/java/de/gedoplan/buch/eedemos/jsf/model/FlowB2Model.java
+++ ee-demos-jsf/src/main/java/de/gedoplan/buch/eedemos/jsf/model/FlowB2Model.java
@@ -16,15 +16,19 @@ public class FlowB2Model extends FlowModelBase
   @FlowDefinition
   public static Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder)
   {
-
+    // Flow-Id setzen
     flowBuilder.id("", "flowB2");
 
+    // Startknoten definieren
     flowBuilder.viewNode("flowB2", "/flowB2/flowB2.xhtml").markAsStartNode();
 
+    // Returnknoten definieren (inkl. seinem Outcome)
     flowBuilder.returnNode("exitFlow").fromOutcome("exitFlowB2");
 
+    // Knoten zum Aufruf von flowB1 definieren
     flowBuilder.flowCallNode("callFlowB1").flowReference("", "flowB1");
 
+    // Navigation für die Rückkehr von flowB1
     flowBuilder.navigationCase().fromViewId("*").fromOutcome("exitFlowB1").toViewId("/flowB2/flowB2.xhtml");
 
     return flowBuilder.getFlow();
