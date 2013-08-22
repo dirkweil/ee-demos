3098b967b42bc85d15be468a3dafee49dcf7c0dc
Dirk Weil <dirk.weil@gedoplan.de>
2013-08-22 08:57:24 +0200

    Externen Flow um Bean ergänzt

---
+++ ee-demos-jsf-extflow/src/main/java/de/gedoplan/buch/eedemos/jsf/model/FlowDModel.java
@@ -0,0 +1,42 @@
+package de.gedoplan.buch.eedemos.jsf.model;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@Named
+@FlowScoped("flowD")
+public class FlowDModel
+{
+  protected Log                   log               = LogFactory.getLog(this.getClass());
+
+  protected String                created;
+
+  private static final DateFormat CREATED_FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+  public String getCreated()
+  {
+    return this.created;
+  }
+
+  @PostConstruct
+  void postConstruct()
+  {
+    this.created = CREATED_FORMATTER.format(new Date());
+    this.log.info(this.created + ": Construct");
+  }
+
+  @PreDestroy
+  void preDestroy()
+  {
+    this.log.info(this.created + ": Destroy");
+  }
+}
