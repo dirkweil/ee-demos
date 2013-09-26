cf58ef3c77547e3a986d28748832b696af0a9745
Dirk Weil <dirk.weil@gedoplan.de>
2013-09-26 16:30:10 +0200

    Mapping korrigiert

--- ee-demos-ejb/src/main/java/de/gedoplan/buch/eedemos/ejb/entity/ShopOrder.java
+++ ee-demos-ejb/src/main/java/de/gedoplan/buch/eedemos/ejb/entity/ShopOrder.java
@@ -9,6 +9,8 @@ import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -17,16 +19,18 @@ import javax.persistence.Table;
 @Access(AccessType.FIELD)
 public class ShopOrder implements Serializable
 {
-  private static final long  serialVersionUID = 1L;
+  private static final long  serialVersionUID     = 1L;
 
-  public static final String TABLE_NAME       = "EJB_SHOPORDER";
+  public static final String TABLE_NAME           = "EJB_SHOPORDER";
+  public static final String TABLE_NAME_SHOPITEMS = "EJB_SHOPORDER_SHOPITEM";
 
   @Id
   @GeneratedValue
   private Integer            id;
 
   @ManyToMany
-  private List<ShopItem>     shopItems        = new ArrayList<>();
+  @JoinTable(name = ShopOrder.TABLE_NAME_SHOPITEMS, joinColumns = @JoinColumn(name = "SHOPORDER_ID"), inverseJoinColumns = @JoinColumn(name = "SHOPITEM_ID"))
+  private List<ShopItem>     shopItems            = new ArrayList<>();
 
   public Integer getId()
   {
