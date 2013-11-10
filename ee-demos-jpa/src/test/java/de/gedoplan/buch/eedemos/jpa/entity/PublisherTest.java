d3705199394f8798a5a549a43da5c39e0066d52e
Dirk Weil <dirk.weil@gedoplan.de>
2013-11-10 17:45:01 +0100

    DataNucleus als JPA-Provider vorbereitet (noch nicht funktional); Entity-Graph-Tests sauberer strukturiert

--- ee-demos-jpa/src/test/java/de/gedoplan/buch/eedemos/jpa/entity/PublisherTest.java
+++ ee-demos-jpa/src/test/java/de/gedoplan/buch/eedemos/jpa/entity/PublisherTest.java
@@ -4,10 +4,10 @@ import de.gedoplan.buch.eedemos.jpa.TestBase;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.AttributeNode;
 import javax.persistence.EntityGraph;
 import javax.persistence.PersistenceUnitUtil;
-
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -23,44 +23,44 @@ import org.unitils.reflectionassert.ReflectionComparatorMode;
 // CHECKSTYLE:OFF
 /**
  * Test der Persistence-Fuktionalität bzgl. der Entity Publisher.
- *
+ * 
  * @author dw
  */
 public class PublisherTest extends TestBase
 {
 
-  public static Publisher testPublisher1 = new Publisher("O'Melly Publishing", "Books", "Music");
-  public static Publisher testPublisher2 = new Publisher("Expert Press", "Books");
-  public static Publisher testPublisher3 = new Publisher("Books reloaded", "Books");
-  public static Publisher[] testPublishers =
-  {
-    testPublisher1, testPublisher2, testPublisher3
-  };
-  public static Person testPerson1 = new Person("Brummer", "Bernd", new MailAddress("brummer", "gmx.de"));
-  public static Person testPerson2 = new Person("Wacker", "Willi", new MailAddress("wacker", "web.de"));
-  public static Person[] testPersons =
-  {
-    testPerson1, testPerson2
-  };
-  public static Book testBook11 = new Book("Better JPA Programs", "12345-6789-0", 340);
-  public static Book testBook12 = new Book("Inside JPA", "54321-9876-X", 265);
-  public static Book testBook13 = new Book("Java and Databases", "11111-2222-6", 850);
-  public static Book testBook21 = new Book("Java for Beginners", "564534-432-2", 735);
-  public static Book testBook22 = new Book("Java vs. C#", "333333-123-0", 145);
-  public static Book testBook23 = new Book("Optimizing Java Programs", "765432-767-8", 230);
-  public static Book testBook30 = new Book("Is there a World after Java?", null, 1);
-  public static Book[] testBooks =
-  {
-    testBook11, testBook12, testBook13, testBook21, testBook22, testBook23, testBook30
-  };
-  public static Book[] testBooksOfPublisher1 =
-  {
-    testBook11, testBook12, testBook13
-  };
-  public static Book[] testBooksOfPublisher2 =
-  {
-    testBook21, testBook22, testBook23
-  };
+  public static Publisher   testPublisher1        = new Publisher("O'Melly Publishing", "Books", "Music");
+  public static Publisher   testPublisher2        = new Publisher("Expert Press", "Books");
+  public static Publisher   testPublisher3        = new Publisher("Books reloaded", "Books");
+  public static Publisher[] testPublishers        =
+                                                  {
+                                                  testPublisher1, testPublisher2, testPublisher3
+                                                  };
+  public static Person      testPerson1           = new Person("Brummer", "Bernd", new MailAddress("brummer", "gmx.de"));
+  public static Person      testPerson2           = new Person("Wacker", "Willi", new MailAddress("wacker", "web.de"));
+  public static Person[]    testPersons           =
+                                                  {
+                                                  testPerson1, testPerson2
+                                                  };
+  public static Book        testBook11            = new Book("Better JPA Programs", "12345-6789-0", 340);
+  public static Book        testBook12            = new Book("Inside JPA", "54321-9876-X", 265);
+  public static Book        testBook13            = new Book("Java and Databases", "11111-2222-6", 850);
+  public static Book        testBook21            = new Book("Java for Beginners", "564534-432-2", 735);
+  public static Book        testBook22            = new Book("Java vs. C#", "333333-123-0", 145);
+  public static Book        testBook23            = new Book("Optimizing Java Programs", "765432-767-8", 230);
+  public static Book        testBook30            = new Book("Is there a World after Java?", null, 1);
+  public static Book[]      testBooks             =
+                                                  {
+                                                  testBook11, testBook12, testBook13, testBook21, testBook22, testBook23, testBook30
+                                                  };
+  public static Book[]      testBooksOfPublisher1 =
+                                                  {
+                                                  testBook11, testBook12, testBook13
+                                                  };
+  public static Book[]      testBooksOfPublisher2 =
+                                                  {
+                                                  testBook21, testBook22, testBook23
+                                                  };
 
   static
   {
@@ -91,7 +91,7 @@ public class PublisherTest extends TestBase
 
   /**
    * Alle Testdaten ausgeben.
-   *
+   * 
    * Dies ist kein Unit-Test im eigentlichen Sinne. Er kann probeweise für eine Ausgabe der Testdaten genutzt werden.
    */
   @Test
@@ -254,38 +254,36 @@ public class PublisherTest extends TestBase
 
   @Test
   //  @Ignore
-  public void testQueryWithSimpleLoadGraph()
+  public void testQueryWithSimpleNamedLoadGraph()
   {
-    System.out.println("----- testQueryWithSimpleLoadGraph -----");
+    System.out.println("----- testQueryWithSimpleNamedLoadGraph -----");
 
     testFetchOrLoad("javax.persistence.loadgraph", "Publisher_books", true, false, false, true);
   }
 
   @Test
   //  @Ignore
-  public void testQueryWithSimpleFetchGraph()
+  public void testQueryWithSimpleNamedFetchGraph()
   {
-    System.out.println("----- testQueryWithSimpleFetchGraph -----");
+    System.out.println("----- testQueryWithSimpleNamedFetchGraph -----");
 
     testFetchOrLoad("javax.persistence.fetchgraph", "Publisher_books", true, false, false, false);
   }
 
   @Test
   //  @Ignore
-  public void testQueryWithSimpleFetchGraphDynamic()
+  public void testQueryWithSimpleDynamicFetchGraph()
   {
-    System.out.println("----- testQueryWithSimpleFetchGraphDynamic -----");
+    System.out.println("----- testQueryWithSimpleDynamicFetchGraph -----");
 
     EntityGraph<Publisher> entityGraph = this.entityManager.createEntityGraph(Publisher.class);
     entityGraph.addAttributeNodes(Publisher_.books.getName());
-    entityManagerFactory.addNamedEntityGraph("Publisher_books(dynamic)", entityGraph);
 
-//    testFetchOrLoad("javax.persistence.fetchgraph", "Publisher_books(dynamic)", true, false, false, false);
     testFetchOrLoad("javax.persistence.fetchgraph", entityGraph, true, false, false, false);
   }
 
   @Test
-  @Ignore("NPE bei HBN")
+  @Ignore("Entity Graph Support ist derzeit noch zu unvollstaendig implementiert, um komplexe Graphen sinnvoll zu testen")
   public void testQueryWithComplexLoadGraph()
   {
     System.out.println("----- testQueryWithComplexLoadGraph -----");
@@ -301,10 +299,8 @@ public class PublisherTest extends TestBase
     {
       if (hintValue instanceof String)
       {
-        // TODO: EclipseLink findet den Entity Graph nicht immer, wenn er nur als Name angegeben wird. Daher separat suchen.
+        // Einige Provider finden den Entity Graph nicht, wenn er nur als Name angegeben wird. Daher separat suchen.
         EntityGraph<?> entityGraph = this.entityManager.getEntityGraph((String) hintValue);
-//        entityGraph.getAttributeNodes();
-
         hintValue = entityGraph;
       }
 
@@ -315,35 +311,34 @@ public class PublisherTest extends TestBase
     System.out.println("Hint: " + hintKey + " = " + hintValue);
 
     Publisher publisher = query.getResultList().get(0);
-    System.out.println(publisher.toDebugString());
-    System.out.println("  id: " + publisher.getId());
-    System.out.println("  name: " + publisher.getName());
 
     PersistenceUnitUtil persistenceUnitUtil = entityManagerFactory.getPersistenceUnitUtil();
-    for (String fieldName : new String[]
-    {
-      "books", "categories"
-    })
-    {
-      System.out.println("  " + fieldName + " loaded: " + persistenceUnitUtil.isLoaded(publisher, fieldName));
-    }
+    //    for (String fieldName : new String[]
+    //    {
+    //      "books", "categories"
+    //    })
+    //    {
+    //      System.out.println("  " + fieldName + " loaded: " + persistenceUnitUtil.isLoaded(publisher, fieldName));
+    //    }
 
     boolean booksLoadedActual = persistenceUnitUtil.isLoaded(publisher, Publisher_.books.getName());
     Assert.assertEquals("publisher.books loaded", booksLoadedExpected, booksLoadedActual);
-    if (booksLoadedExpected)
+
+    if (booksLoadedActual && !publisher.getBooks().isEmpty())
     {
-      for (Book book : publisher.getBooks())
+      Book book = publisher.getBooks().get(0);
+      boolean authorsLoadedActual = persistenceUnitUtil.isLoaded(book, Book_.authors.getName());
+      Assert.assertEquals("books.authors loaded", authorsLoadedExpected, authorsLoadedActual);
+
+      if (authorsLoadedActual && !book.getAuthors().isEmpty())
       {
-        Assert.assertEquals("books.authors loaded", authorsLoadedExpected, persistenceUnitUtil.isLoaded(book, Book_.authors.getName()));
-        if (authorsLoadedExpected)
-        {
-          for (Person person : book.getAuthors())
-          {
-            Assert.assertEquals("person.mailAddresses loaded", mailAddressesLoadedExpected, persistenceUnitUtil.isLoaded(person, Person_.mailAddresses.getName()));
-          }
-        }
+        Person person = book.getAuthors().iterator().next();
+        boolean mailAddressesLoadedActual = persistenceUnitUtil.isLoaded(person, Person_.mailAddresses.getName());
+        Assert.assertEquals("person.mailAddresses loaded", mailAddressesLoadedExpected, mailAddressesLoadedActual);
       }
     }
-    Assert.assertEquals("publisher.categories loaded", categoriesLoadedExpected, persistenceUnitUtil.isLoaded(publisher, Publisher_.categories.getName()));
+
+    boolean categoriesLoadedActual = persistenceUnitUtil.isLoaded(publisher, Publisher_.categories.getName());
+    Assert.assertEquals("publisher.categories loaded", categoriesLoadedExpected, categoriesLoadedActual);
   }
 }
